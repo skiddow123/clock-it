c14170628a830ddce9091ffdc81f4e99b910551a
Abdul Faheem Kassim <38706000+skiddow123@users.noreply.github.com>
2023-01-08 02:03:02 +0000

    edited infoTable component to incluse status for parts usage

--- src/components/infoTable/InfoTable.js
+++ src/components/infoTable/InfoTable.js
@@ -1,90 +1,105 @@
 import React from 'react'
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper, Container, Typography } from '@mui/material'
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper, Container, Typography, Button, TableContainer } from '@mui/material'
 import './InfoTable.css'
+import { borderLeft, color } from '@mui/system'
 
 export default function InfoTable() {
+const tableHeaderStyle ={
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "#002984"
+}
+
+const toBeUsedStyle = {
+    fontWeight: "bold",
+    fontSize: "0.75rem",
+    color: "white",
+    backgroundColor: "#4c8c4a",
+    borderRadius: 8,
+    padding: "3px 10px", 
+    display: "inline-block"
+}
+
+const notToBeUsedStyle = {
+    fontWeight: "bold",
+    fontSize: "0.75rem",
+    color: "white",
+    backgroundColor: "#ba000d",
+    borderRadius: 8,
+    padding: "3px 10px", 
+    display: "inline-block"
+}
+
     return (
         <div className='infotable'>
-            <Container component={Paper} elevation={3}>
-                <Table aria-label="simple table">
+            <TableContainer component={Paper} elevation={3}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>eRTG/RTG EQUIPMENT FAULTS</TableCell>
-                            <TableCell>NOT TO BE USED</TableCell>
-                            <TableCell>TO BE USED</TableCell>
-                            <TableCell>ACTION</TableCell>
+                            <TableCell style={tableHeaderStyle}>eRTG/RTG EQUIPMENT FAULTS</TableCell>
+                            <TableCell style={tableHeaderStyle}>USAGE STATUS</TableCell>
+                            <TableCell style={tableHeaderStyle}>ACTION</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         <TableRow>
                             <TableCell>Faulty Camera</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for replacement</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>PA System or Horn or Intercom not working</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for replacement</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Seat can't adjust</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for repair</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>GPS not working</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for repair</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Broken wiper</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for replacement</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Joystick Cover off</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for repair</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Spreader Indicator Lights not working</TableCell>
-                            <TableCell><Typography style={{color: "#FFCCCB"}}>Not to be used</Typography></TableCell>
-                            <TableCell>{null}</TableCell>
+                            <TableCell><Typography style={notToBeUsedStyle}>Not to be used</Typography></TableCell>
                             <TableCell>Report for repair</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Very Poor Visibilit</TableCell>
-                            <TableCell>Not to be used</TableCell>
-                            <TableCell>{null}</TableCell>
+                            <TableCell><Typography style={notToBeUsedStyle}>Not to be used</Typography></TableCell>
                             <TableCell>Report for cleaning</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>DGPS not working</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for repair</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Wheel Projector not present or damaged</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Report for replacement</TableCell>
                         </TableRow>
                         <TableRow>
                             <TableCell>Worn out key pads</TableCell>
-                            <TableCell>{null}</TableCell>
-                            <TableCell>To be used</TableCell>
+                            <TableCell><Typography style={toBeUsedStyle}>To be used</Typography></TableCell>
                             <TableCell>Inform IT</TableCell>
                         </TableRow>
                     </TableBody>
                 </Table>
-            </Container>
+            </TableContainer>
         </div>
     )
 }
